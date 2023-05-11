b3292c7944300b9589671fc3a01cea05b029c718
gustirizkia <gustidoang936@gmail.com>
2023-05-11 21:30:26 +0700

    change preload

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -622,7 +622,6 @@ Alasan utamanya karena kepuasaan customer adalah yang utama bagi kami</p>
     </div>
   </footer><!-- End Footer -->
 
-  <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
